2d278307b9bd4c628a5b4ed90a5b80f126ab1b73
Nisan <nisan@webiks.com>
2023-01-08 15:30:38 +0200

    ready to test

--- my-app/src/components/colorPicker/colorPicker.tsx
+++ my-app/src/components/colorPicker/colorPicker.tsx
@@ -34,107 +34,73 @@ interface ColorPickerProps{
     setColor?: (newColor: string)=>void;
 }
 
-const DEFAULT_COLOR = '#7fffae';
+const DEFAULT_COLOR = '#2d37f6';
 
 
-const ColorPicker: FC<ColorPickerProps> = ({currentColor=DEFAULT_COLOR,setColor})=> {
+const ColorPicker: FC<ColorPickerProps> = ({currentColor="#FF37f9",setColor})=> {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const stripRef = useRef<HTMLCanvasElement>(null);
     const selectedColorRef = useRef<HTMLDivElement>(null);
     const selectedColor = useRef<string>('');
     const isMouseDown = useRef<boolean>(false);
 
-    const {width, height} = {width:500, height:400}
-    const {sWidth, sHeight} = {sWidth: 500, sHeight:15}
+    const {width, height} = {width:300, height:300}
     const pickerCircle = {x:100 , y:100, width: 7 , height: 7};
-    const stripCircle = {x:100 , y:7, width: 7.5 , height: 7.5};
     
     useEffect(()=>{
-        // draw palette
         initPalette();
+        displayInitColor();
 
+        canvasRef.current?.addEventListener('mousedown',onMouseDown);
+        canvasRef.current?.addEventListener('mousemove',onMouseMove);
+        canvasRef.current?.addEventListener('mouseup',onMouseUp);
+    },[]);
+
+    const displayInitColor = ()=>{
         const rgbColor = hexToRgb(currentColor);
         if(rgbColor){
             const coordinates = findColorInCanvas(rgbColor);
             pickerCircle.x = coordinates?.x || 0;
             pickerCircle.y = coordinates?.y || 0;
         }
+
         if(selectedColorRef.current)
             selectedColorRef.current.style.backgroundColor = `rgb(${rgbColor?.r}, ${rgbColor?.g}, ${rgbColor?.b})` ;
         drawCircle();
-        createStrip();
-
-        // Listener to event
-        canvasRef.current?.addEventListener('mousedown',onMouseDown);
-        canvasRef.current?.addEventListener('mousemove',onMouseMove);
-        canvasRef.current?.addEventListener('mouseup',onMouseUp);
-
-    },[]);
-
-    const createStrip = ()=>{
-        const context =  stripRef.current?.getContext('2d');
-
-        /* canvasRef.current?.addEventListener('mousedown',onMouseDown);
-        canvasRef.current?.addEventListener('mousemove',onMouseMove);
-        canvasRef.current?.addEventListener('mouseup',onMouseUp); */
-        if(!context) return;
-        let gradient = context?.createLinearGradient(0,0, width, 0);
-
-        // Add basic colors
-        gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
-        gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
-        gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
-        gradient.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
-        gradient.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
-        gradient.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
-        gradient.addColorStop(1, 'rgba(255, 0, 0, 1)');
-
-        context.fillStyle = gradient;
-        context.fillRect(0,0, sWidth, sHeight);
-
-        context.beginPath();
-        context.arc(stripCircle.x, stripCircle.y,stripCircle.width, 0, Math.PI * 2);
-        context.strokeStyle = "black";
-        context.stroke();
-        context.closePath();
-
-
+        
     }
-
 
     const findColorInCanvas = (rgbColor: RGBcolor)=>{
         const context =  canvasRef.current?.getContext('2d');
         const imageData = context?.getImageData(0, 0,width,height);
         if(!imageData) return
         let i;
-        // [12, 12, 12, 12, 11, 11, 11, 11 ...]
         for(i = 0 ;i<imageData?.data.length/4 ;i++){
             const pixelRGBA = imageData.data.slice(i*4,i*4+4);
             if(rgbColor.r === pixelRGBA[0] && rgbColor.g === pixelRGBA[1] && rgbColor.b === pixelRGBA[2])
                 break;
         }
-        
         const x = i% width;
         const y = Math.floor(i / width );
 
         return {x,y};
     }
 
-    const hexToRgb = (hex:string)=> {
+    const hexToRgb = (hex:string) : RGBcolor=> {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result ? {
           r: parseInt(result[1], 16),
           g: parseInt(result[2], 16),
           b: parseInt(result[3], 16)
-        } : null;
+        } : hexToRgb(DEFAULT_COLOR);
       }
 
     const getPickedColor = ()=>{
         const context =  canvasRef.current?.getContext('2d');
         const imageData = context?.getImageData(pickerCircle.x, pickerCircle.y,1,1);
-        return {r: imageData?.data[0], g:imageData?.data[1], b:imageData?.data[2]}  
+        return {r: imageData?.data[0], g:imageData?.data[1], b:imageData?.data[2]};
     }
+
 
     const onMouseUp = ()=>{
         isMouseDown.current = false;
@@ -186,18 +152,6 @@ const ColorPicker: FC<ColorPickerProps> = ({currentColor=DEFAULT_COLOR,setColor}
         if(!context) return;
         let gradient = context?.createLinearGradient(0,0, width, 0);
 
-        // Add basic colors
-        /* const imageData = context.createImageData(width, height);
-        for (var i = 0; i < imageData.data.length; i += 4) {
-            var brightness = i / (imageData.data.length - 1);
-            imageData.data[i] = brightness * 255;
-            imageData.data[i + 1] = brightness * 255;
-            imageData.data[i + 2] = brightness * 255;
-            imageData.data[i + 3] = 255;
-        }
-        context.putImageData(imageData, 0, 0); */
-
-
         gradient.addColorStop(0, 'rgba(255, 0, 0, 1)');
         gradient.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
         gradient.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
@@ -209,7 +163,6 @@ const ColorPicker: FC<ColorPickerProps> = ({currentColor=DEFAULT_COLOR,setColor}
         context.fillStyle = gradient;
         context.fillRect(0,0, width, height);
 
-        // Apply black and white
         gradient = context?.createLinearGradient(0,0, 0, height);
         gradient?.addColorStop(0, 'rgba(255, 255,255,1)');
         gradient?.addColorStop(0.5, 'rgba(255, 255,255,0)');
